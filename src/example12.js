604454cd764cbcfef4c241cb7fcdcd805bb04bf4
Vanya Usalko <dk4aol@gmail.com>
2024-06-23 16:29:21 +0300

    # One more tests.

--- src/example12.js
+++ src/example12.js
@@ -1,6 +1,6 @@
-export function example12(input) {
-    if (typeof input !== 'string') {
-        throw 'Unsupported type of input (this function support only string type)'
+export function example12(dateTime) {
+    if (typeof dateTime !== 'Date') {
+        throw 'Unsupported type of input (this function support only Date type)'
     }
     return input.split(' ').map((x) => x.slice(0, 1).toUpperCase() + x.substring(1)).join('')
 }
--- tests/example12.test.js
+++ tests/example12.test.js
@@ -2,7 +2,12 @@ import { expect } from 'chai'
 import example12 from '../src/example12.js'
 
 describe('#example12', function () {
-   it ('', function() {
-    expect(example12('JavaScript Exercises')).to.equal('JavaScriptExercises')
+   it ('Write a JavaScript program to display the current day and time in the following format.', function() {
+    const dateTime = new Date()
+    const weekday = Intl.DateTimeFormat('ru-RU', {weekday: 'long'}).format(dateTime)
+
+    console.log(`${result}`)
+    expect(example12()).to.equal(`Today is : ${weekday}
+Current time is : 10 PM : 30 : 38`)
 })
 })
